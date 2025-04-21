4de1043286f36f3bdbfe5e59c6b0cbfa2df39251
Kauan <h6671992@gmail.com>
2025-04-21 18:45:17 -0300

    Add files via upload

---
+++ cadastro.py
@@ -0,0 +1,24 @@
+print ("............PREENCHA O FORMULÁRIO.............")
+
+# Solicitando as informações do usuario
+
+nome = input ("DIGITE O SEU NOME:")
+idade = float (input ("DIGITE A SUA IDADE: ")) 
+sexo = input ("DIGITE O SEU SEXO: ")
+rua = input ("DIGITE A SUA RUA: ")
+bairro = input ("DIGITE O SEU BAIRRO: ")
+cidade = input ("DIGITE A SUA CIDADE: ")
+
+
+# Exibindo as informações inseridas
+
+print ("..........SEUS DADOS ABAIXO...........")
+
+print ("O SEU NOME É: ", nome)
+print ("SUA IDADE É: ", idade)
+print ("SEU SEXO É: ", sexo)
+print ("SUA RUA É: ", rua)
+print ("SEU BAIRRO É ", bairro)
+print ("SUA CIDADE É: ", cidade)
+
+print ("SEUS DADOS ESTÃO CORRETOS?: ")
